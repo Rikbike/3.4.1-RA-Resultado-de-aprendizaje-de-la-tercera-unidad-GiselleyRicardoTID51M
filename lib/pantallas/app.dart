8d2839854e70da4a56ad09acb9a54a58ec1d503b
Rikbike <145867755+Rikbike@users.noreply.github.com>
2024-07-27 17:41:37 -0600

    Update app.dart

--- lib/pantallas/app.dart
+++ lib/pantallas/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'home.dart'; // Importa el home.dart
+import 'home.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -11,7 +11,7 @@ class App extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Home(), // Usa la pantalla Home
+      home: const Home(),
       routes: {
       },
     );
